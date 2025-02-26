fc71a9e0a2b0b3a94683d6dee0400c922701d84e
nooorf <nfatima.bscs23seecs@seecs.edu.pk>
2025-02-26 22:51:59 +0500

    Added component to clear search form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -13,8 +13,7 @@ const SearchForm = ({query}: {query?: string}) => {
             placeholder="Search for startups"
             className='search-input' />
         
-       <div className='flex gap-2'>
-            
+       <div className='flex gap-2'>          
             <button type="submit" className='search-btn text-white'>
                 <Search className='size-5'/>
             </button>
