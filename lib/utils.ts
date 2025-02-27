1a73fd06efaeae7b25bf4f567818997b6a292d67
nooorf <nfatima.bscs23seecs@seecs.edu.pk>
2025-02-27 12:18:00 +0500

    Added format date utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function formatDate(date: string | Date) {
+  if(!date) return 'Invalid date';
+
+  const parsedDate = typeof date === 'string' ? new Date(date) : date;
+
+  if(isNaN(parsedDate.getTime())) return 'Invalid date';
+  
+  return new Date(date).toLocaleDateString(
+    'en-US',
+    {  month: 'long', day: 'numeric' , year: 'numeric'}
+  )
+}
