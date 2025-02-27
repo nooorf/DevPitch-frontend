1de1622a74c3521d63fdf1588af71dee56f25466
nooorf <nfatima.bscs23seecs@seecs.edu.pk>
2025-02-27 12:43:17 +0500

    created component to display view count on post page

---
+++ components/View.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Ping from "./Ping";
+
+const View = ({ id }: { id: string }) => {
+  const [views, setViews] = useState<number>(0);
+
+  function formatNumber(number: number) {
+    return number === 1 ? `${number} view` : `${number} views`;
+  }
+
+  useEffect(() => {
+    const incrementViews = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/api/posts/:id');
+
+        if (!response.ok) {
+          throw new Error("Failed to update views");
+        }
+
+        const data = await response.json();
+        setViews(data.views);
+      } catch (error) {
+        console.error("Error updating views:", error);
+      }
+    };
+
+    incrementViews();
+  }, [id]);
+
+  return (
+    <div className="view-container">
+      <div className="absolute -top-2 -right-2">
+        <Ping />
+      </div>
+      <p className="view-text">
+        <span className="font-black">{formatNumber(views)}</span>
+      </p>
+    </div>
+  );
+};
+
+export default View;
