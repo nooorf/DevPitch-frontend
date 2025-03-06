becec4aa7ed86f85efde6406c99fd0869935904d
nooorf <nfatima.bscs23seecs@seecs.edu.pk>
2025-03-06 19:32:22 +0500

    Created component to display reported posts

---
+++ components/ModeratorDisplay.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import PostCard from './PostCard'
+
+const ModeratorDisplay = async () => {
+    try {
+        const res = await fetch(`http://localhost:5000/posts/reported`, {
+          cache: "no-store", 
+        });
+    
+        if (!res.ok) throw new Error("Failed to fetch posts");
+    
+        const startups = await res.json();
+    
+        return (
+          <>
+            <ul>
+              {startups.length > 0 ? (
+                startups.map((startup: any) => <PostCard key={startup._id} post={startup} />)
+              ) : (
+                <p>No posts have been reported yet</p>
+              )}
+            </ul>
+          </>
+        );
+    } catch (error) {
+        return <p className="text-red-500">Error fetching posts: {(error as Error).message}</p>;
+    }
+};
+export default ModeratorDisplay
