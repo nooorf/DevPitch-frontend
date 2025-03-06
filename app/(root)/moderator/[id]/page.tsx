e62a4d3c7acb328566bb1efb29887764a9b5c505
nooorf <nfatima.bscs23seecs@seecs.edu.pk>
2025-03-06 19:25:00 +0500

    Set up moderator panel

--- app/(root)/moderator/[id]/page.tsx
+++ app/(root)/moderator/[id]/page.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 import Image from 'next/image';
 import UserProfile from '@/components/UserProfile';
 
-interface User {
+interface Moderator {
     _id: string;
     name: string;
     githubUsername: string;
     profilePicture: string;
     bio?: string;
+    role: string;
 }
-const getUser = async (id: string): Promise<User | null> => {
+const getUser = async (id: string): Promise<Moderator | null> => {
     try {
         const res = await fetch(`http://localhost:5000/users/${id}`, { cache: "no-store" });
         if (!res.ok) throw new Error("Failed to fetch user");
@@ -21,32 +22,34 @@ const getUser = async (id: string): Promise<User | null> => {
     }
 }
 const page = async ({ params }: { params: {id: string} }) => {
-    const user = await getUser(params.id);
-
-    if(!user) {return <p className='text-red-500'>User not found</p>}
+    const moderator = await getUser(params.id);
+    if(!moderator) {return <p className='text-red-500'>Moderator not found</p>}
+    if(moderator.role !== 'moderator') {
+        return <p className='text-red-500'>You are not authorised to access this page.</p>
+    }
   return (
    <>
     <section className='profile_container'>
         <div className='profile_card'>
             <div className='profile_title'>
                 <h3 className='text-24-black uppercase text-center line-clamp-1'>
-                    {user.name}
+                    {moderator.name}
                 </h3>
             </div>
-            <Image src={user.profilePicture || "default-avatar.png"}  
-                    alt={user.name} 
+            <Image src={moderator.profilePicture || "default-avatar.png"}  
+                    alt={moderator.name} 
                     width={220} 
                     height={220} 
                     className="profile_image"
             />
-            <p className='text-30-extrabold mt-7 text-center'>@{user?.githubUsername}</p>
-            <p className='mt-1 text-center text-14-normal'>{user?.bio}</p>
+            <p className='text-30-extrabold mt-7 text-center'>@{moderator?.githubUsername}</p>
+            <p className='mt-1 text-center text-14-normal'>{moderator?.bio}</p>
 
         </div>
         <div className='flex-1 flex flex-col gap-5 lg:-mt-5'>
-            <p className='text-30-bold'>All Posts</p>
+            <p className='text-30-bold'>Reported Posts</p>
             <ul className='card_grid-sm'>
-                    <UserProfile id={user._id}/>
+                    {/** Display reported posts */}
             </ul>
         </div>
     </section>
