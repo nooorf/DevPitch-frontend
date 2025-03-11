bb3beed0a3ed6cf79d8e34b203e0d59357662f35
nooorf <nfatima.bscs23seecs@seecs.edu.pk>
2025-03-11 23:02:12 +0500

    Added auth hook

---
+++ hooks/useAuth.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+interface User {
+  _id: string;
+  name: string;
+  role: string; 
+}
+
+export default function useAuth() {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/auth/me", {
+          credentials: "include", 
+        });
+
+        if (!res.ok) throw new Error("Failed to fetch user");
+
+        const data = await res.json();
+        setUser(data);
+      } catch (err) {
+        console.error("Error fetching user:", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  return { user, loading };
+}
